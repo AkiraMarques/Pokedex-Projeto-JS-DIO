2e59963013c1f1ea31855d6c06af16430450185d
AkiraMarques <marqueslucas504@gmail.com>
2023-10-13 18:14:36 -0300

    atualização do script

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,11 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     // Pegar a URL da API solicitada (lista de pokemons)
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
@@ -9,17 +14,10 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     return fetch(url)
         .then((response) => response.json()) // converte o body em json e na linha abaixo printa ele
         .then((jsonBody) => jsonBody.results) // pega o results (que é a lista de pokemon)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // mapeia a lista atras das urls de detalhes
+        .then((detailRequests) => Promise.all(detailRequests)) // executa a promessa de cada uma das listas de detalhes dos pokemons
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
     // sempre o que vai pro segundo then é o retorno do primeiro (encadeamento de then)
 }
-
-// faz uma lista de novas requisições de api's
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-]).then((results) => {
-    console.log(results)
-})
 
